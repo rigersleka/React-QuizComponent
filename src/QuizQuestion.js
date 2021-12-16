da4b6ee61f4283ac414ecfd09e8b7ff3f84f72c9
Rigers Leka <rigers.leka@sisal.al>
2021-12-16 15:30:36 +0100

    update quizQuestion in classBased component

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const QuizQuestion = (props) => {
-  return (
-    <main>
-      <section>
-        <p>
-          {props.quiz_question.instruction_text}
-        </p>
-      </section>
-      <section className='buttons'>
-        <ul>
-          <li>{props.quiz_question.answer_options[0]}</li>
-        </ul>
-      </section>
-    </main>
-  );
-};
+class QuizQuestion extends Component {
+  render() {
+    return (
+      <main>
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className='buttons'>
+          <ul>
+            <li>{this.props.quiz_question.answer_options[0]}</li>
+          </ul>
+        </section>
+      </main>
+    );
+  }
+}
 
 export default QuizQuestion;
