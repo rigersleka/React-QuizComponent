72f88f313f17edace8c97759475fe6de1a3286b9
Rigers Leka <rigers.leka@sisal.al>
2021-12-17 14:59:45 +0100

    display error message for incorrectAnswer

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,18 +2,46 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component {
-  handleClick(buttonText) {
-    if (buttonText === this.props.quiz_question.answer) {
-      this.props.showNextQuestionHandler()
-    }
-    console.log('clicked button');
+  constructor(props) {
+    super(props);
+    this.state = {
+      incorrectAnswer: false,
+    };
   }
 
+  handleClick(buttonText) {
+    if (buttonText === this.props.quiz_question.answer) {
+      this.setState({ incorrectAnswer: false });
+      this.props.showNextQuestionHandler();
+    } else {
+      this.setState({ incorrectAnswer: true });
+    }
+
+    console.log(this.state.incorrectAnswer);
+  }
+
+  /*
+    Most types of implementing Conditional_Rendering:
+        1. Ternary Operator
+        2. Using logigal &&
+
+        (refenrece: https://www.digitalocean.com/community/tutorials/7-ways-to-implement-conditional-rendering-in-react-applications)
+  */
   render() {
+    //Ternary Operator
+    const displayIncorrectAnswer = this.state.incorrectAnswer ? (<p className='error'>Sorry, that's not right.</p>): null;
+   
     return (
       <main>
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
+          <div>
+            {displayIncorrectAnswer}
+          </div>
+         {/* Conditional Rendering */}
+          <div>
+            {this.state.incorrectAnswer && (<p className='error'>TESTTEST</p>)}
+          </div> 
         </section>
         <section className='buttons'>
           <ul>
@@ -35,4 +63,3 @@ class QuizQuestion extends Component {
 }
 
 export default QuizQuestion;
-
